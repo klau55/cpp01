670d3a65cbb40f30a042d97df9c4d1042f50f9b6
klau55 <kipatok@bk.ru>
2025-02-12 18:12:16 +0200

    cleaner

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,17 +1,11 @@
 #include "Zombie.hpp"
 
 int main (void) {
-    Zombie first("first");
-    Zombie *second = newZombie("second");
-    first.announce();
-    second->announce();
-    randomChump("third");
     int n = 5;
     Zombie *horde = zombieHorde(n, "horde");
     for (int i = 0; i < n; i++) {
         horde[i].announce();
     }
-    delete second;
     delete[] horde;
 
     return 0;
