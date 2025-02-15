30667335031643d5bf5fcfb51b6951e755a2980c
nkarpilo <nkarpilo@student.hive.fi>
2025-02-15 15:08:29 +0200

    qwead

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -9,7 +9,7 @@ public:
     Weapon(std::string type);
     ~Weapon();
     const std::string& getType();
-    void setType(std::string& type);
+    void setType(std::string type);
 private:
     std::string type;
 };
